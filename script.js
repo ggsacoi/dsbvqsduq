e2e18041b2521da377370f66ff2a6448f907ea8a
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2025-03-05 15:39:20 +0100

    zfzeofihaepif

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-let video = document.querySelector('video');
-if(navigator.mediaDevices.getUserMedia) {
-    navigator.mediaDevices.getUserMedia({ video: true })
-    .then(function(stream) {
-        video.srcObject = stream;
-    })
-    .catch(function(error) {
-        console.log("Error: " + error);
-    });
-}
+// let video = document.querySelector('video');
+// if(navigator.mediaDevices.getUserMedia) {
+//     navigator.mediaDevices.getUserMedia({ video: true })
+//     .then(function(stream) {
+//         video.srcObject = stream;
+//     })
+//     .catch(function(error) {
+//         console.log("Error: " + error);
+//     });
+// }
 navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
     // Show a map centered at latitude / longitude.
 
   map.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Co6J330sC18" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
+});
+addEventListener("DOMContentLoaded", () => {
+    const toggle = document.getElementById("snap");
+    const theme = localStorage.getItem('theme');
+    const audio = document.querySelector('audio');
+    if(theme) {
+        document.documentElement.setAttribute('data-theme', theme);
+        if(theme === 'dark') {
+            toggle.checked = true;
+        }
+    }
+    toggle.addEventListener("change", () => {
+        const newTheme = toggle.checked ? "dark" : "light";
+        document.documentElement.setAttribute("data-theme", newTheme);
+        localStorage.setItem("theme", newTheme);
+        audio.play();
+    });
 });
