23977f32987fe27fe0c8864c03417f5afcde3ccf
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2025-03-05 14:46:04 +0100

    fszhnoiueafjhe

--- script.js
+++ script.js
@@ -12,5 +12,5 @@ navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
     // Show a map centered at latitude / longitude.
 
-  map.innerHTML = '<iframe width="100%" height="100%" src="https://maps.google.com/maps?q='+latitude+','+longitude+'&amp;z=15&amp;output=embed"></iframe>';
+  map.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Co6J330sC18" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
 });
