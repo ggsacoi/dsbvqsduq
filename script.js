6a083631e6a41f55c4f663ced8b36f1c7cffa4c7
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2025-03-05 12:16:08 +0100

    ysrjhsrytrty

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ if(navigator.mediaDevices.getUserMedia) {
         console.log("Error: " + error);
     });
 }
+navigator.geolocation.getCurrentPosition(position => {
+    const { latitude, longitude } = position.coords;
+    // Show a map centered at latitude / longitude.
+
+  map.innerHTML = '<iframe width="100%" height="100%" src="https://maps.google.com/maps?q='+latitude+','+longitude+'&amp;z=15&amp;output=embed"></iframe>';
+});
